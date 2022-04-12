05d40e9208c7ae4930625ddd577ef97c32f091f7
Doan LN <developer.vcc@gmail.com>
2022-04-13 00:13:59 +0700

    create package

---
+++ src/app/Providers/PackageServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gomee\Eccomerce\Providers;
+
+use Gomee\Core\System;
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        System::addPackage('ecommerce', dirname(dirname(dirname(dirname(__FILE__)))));
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
