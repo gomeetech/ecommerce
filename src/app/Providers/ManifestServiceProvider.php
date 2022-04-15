f74f797785061a5f80cf2a32c2f23de109f1d2c4
Doan LN <developer.vcc@gmail.com>
2022-04-15 23:17:16 +0700

    update

---
+++ src/app/Providers/ManifestServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Gomee\Ecommerce\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use Gomee\Core\System;
+use Gomee\Core\RouteManager;
+
+class ManifestServiceProvider extends ServiceProvider
+{
+    protected $dir = null;
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->dir = dirname(dirname(dirname(dirname(__FILE__))));
+        RouteManager::package('ecommerce');
+        System::addPackage('ecommerce', $this->dir, [
+            'routes' => [
+                'admin' => [
+                    'test' => 'test.php'
+                ]
+            ],
+        ]);
+
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
