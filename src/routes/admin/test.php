140b2f9d9169f74e5c66eac08c69bbf7bb621498
Doan LN <developer.vcc@gmail.com>
2022-04-18 23:10:47 +0700

    cap nhat

--- src/routes/admin/test.php
+++ src/routes/admin/test.php
@@ -1,8 +1,11 @@
 <?php
 
+use Gomee\Ecommerce\Controllers\Admin\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(Request $request){
     return $request->all();
 });
+
+Route::get('demo', [TestController::class, 'getTestView'])->name('demo');
